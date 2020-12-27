88892e23a0b589dd3d5c2c944a59949a08f0e5bc
Faith Magras <bdanielle092@gmail.com>
2020-12-27 15:35:02 -0600

    made a guessing game

---
+++ Assets/Script/GuessTheNumberGame.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GuessTheNumberGame : MonoBehaviour {
+    public InputField input;
+    public Text infoText;
+
+    private int guessNumber;
+    private int usersGuess;
+
+	// Use this for initialization
+	void Start () {
+        guessNumber = Random.Range(0, 100);
+	}
+	
+	public void CheckGuess()
+    {
+        usersGuess = int.Parse(input.text);
+
+        if(usersGuess == guessNumber)
+        {
+            infoText.text = "You Guess the Number, Congrats!";
+        }else if( usersGuess > guessNumber)
+        {
+            infoText.text = "Your Number is Greater than the Guess Number";
+        }else if( usersGuess < guessNumber)
+        {
+            infoText.text = "Your Number is lower than the Guess Number";
+        }
+        input.text = "";
+    }
+	
+	
+}
